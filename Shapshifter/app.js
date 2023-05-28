c16e6e30b24f2b549512319d6b697aad59549832
Muhammad-Bilal321 <128062466+Muhammad-Bilal321@users.noreply.github.com>
2023-05-29 00:26:06 +0500

    ShapeShifter

---
+++ Shapshifter/app.js
@@ -0,0 +1,18 @@
+function circleShape(){
+    var circleShape = document.getElementById("Shapshifter");
+    circleShape.className = "circle"
+}
+function squareShape(){
+var squareShape = document.getElementById("Shapshifter");
+squareShape.className = "square"
+}
+function rectangleShape(){
+    var rectangleShape = document.getElementById("Shapshifter");
+    rectangleShape.className = "rectangle"
+}
+function triangleShape(){
+    var triangleShape = document.getElementById("Shapshifter");
+    triangleShape.className = "triangle"
+}
+
+// https://shape-shifter-1d327.web.app
